232d676e8a93a0a86a7759c913f347f6e80ceca5
Jasper <49565505+wluxie@users.noreply.github.com>
2022-10-28 21:52:25 -0700

    Update main-window

--- NFL-Pamphlet/mainwindow.cpp
+++ NFL-Pamphlet/mainwindow.cpp
@@ -1,9 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QWidget(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
@@ -12,3 +10,37 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_filterButton_clicked()
+{
+    filterWindow = new FilterWindow(this);
+    filterWindow->show();
+}
+
+
+void MainWindow::on_helpButton_clicked()
+{
+    helpWindow = new HelpWindow(this);
+    helpWindow->show();
+}
+
+
+void MainWindow::on_contactButton_clicked()
+{
+    contactWindow = new ContactWindow(this);
+    contactWindow->show();
+}
+
+
+void MainWindow::on_loginButton_clicked()
+{
+    adminWindow = new AdminWindow(this);
+    adminWindow->show();
+}
+
+
+void MainWindow::on_refreshButton_clicked()
+{
+    
+}
+
--- NFL-Pamphlet/mainwindow.h
+++ NFL-Pamphlet/mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QWidget>
+#include "helpwindow.h"
+#include "helpwindow.h"
+#include "filterwindow.h"
+#include "contactwindow.h"
+#include "adminwindow.h"
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +20,24 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_filterButton_clicked();
+
+    void on_helpButton_clicked();
+
+    void on_contactButton_clicked();
+
+    void on_loginButton_clicked();
+
+    void on_refreshButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    
+    HelpWindow *helpWindow;
+    FilterWindow *filterWindow;
+    ContactWindow *contactWindow;
+    AdminWindow *adminWindow;
 };
 
 #endif // MAINWINDOW_H
