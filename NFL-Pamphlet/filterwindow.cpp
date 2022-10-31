b8aa07cedd02b510b05293512e9dce9c20527940
Bridget Viravouth <bviravouth0@saddleback.edu>
2022-10-30 21:17:00 -0700

    set attribute to delete on close (filter window)

--- NFL-Pamphlet/filterwindow.cpp
+++ NFL-Pamphlet/filterwindow.cpp
@@ -10,6 +10,13 @@ FilterWindow::FilterWindow(QWidget *parent) :
     ui->comboBox->addItem("American Football Confrence");
 }
 
+void FilterWindow::on_filterButton_clicked()
+{
+   filterWindow = new FilterWindow(this);
+   filterWindow->setAttribute(Qt::WA_DeleteOnClose);
+   filterWindow->show();
+}
+
 FilterWindow::~FilterWindow()
 {
     delete ui;
