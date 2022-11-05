fee4d2730b391c569d4f91006eb8e157f9760dce
Jasper <49565505+wluxie@users.noreply.github.com>
2022-11-05 14:57:52 -0700

    Revert "ITS WORKSKKSKSKSKKS"
    This reverts commit 4b1e88a001a6c913674dc3573943eaef4821e15a.

--- NFL-Pamphlet/teamdata.cpp
+++ NFL-Pamphlet/teamdata.cpp
@@ -8,19 +8,11 @@ NFLData::TeamData::TeamData(string teamName, string stadiumName, int capacity, s
 NFLData::TeamData::~TeamData() { }
 
 string NFLData::TeamData::getTeamName() { return teamName; }
-
 string NFLData::TeamData::getStadiumName() { return stadiumName; }
-
 int NFLData::TeamData::getCapacity() { return capacity; }
-
 string NFLData::TeamData::getLocation() { return location; }
-
 string NFLData::TeamData::getConference() { return (const string[]){"National Football Conference", "American Football Conference"}[conference]; }
-
 string NFLData::TeamData::getDivision() { return (const string[]){"North", "South", "East", "West"}[division]; }
-
 string NFLData::TeamData::getSurfaceType() { return surfaceType; }
-
 string NFLData::TeamData::getRoofType() { return (const string[]){"Open", "Retractable", "Fixed"}[roofType]; }
-
 int NFLData::TeamData::getYearOpened() { return yearOpened; }
