3c184bd66dd364d274fc5fc3f06e617dbb58efc8
bridget127 <115428836+bridget127@users.noreply.github.com>
2022-11-07 23:56:52 -0800

    im so done for

--- NFL-Pamphlet/mainwindow.h
+++ NFL-Pamphlet/mainwindow.h
@@ -36,6 +36,7 @@ class MainWindow : public QWidget, public DisplayData
         void on_loginButton_clicked();
         void on_refreshButton_clicked();
         void on_dropdownBox_clicked();
+        void on_dropdownBox_activated();
 
     public:
         explicit MainWindow(QWidget *parent = nullptr);
